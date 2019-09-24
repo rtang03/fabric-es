ff849e5dca73172b8845f99aea294baa84b23771
tangross <rt.do.it@gmail.com>
2019-09-24 23:42:08 +0800

    done refactoring for fabric-cqrs to using with CA enabled

--- packages/fabric-cqrs/src/services/__tests__/service.privatedata.integration.test.ts
+++ packages/fabric-cqrs/src/services/__tests__/service.privatedata.integration.test.ts
@@ -1,51 +1,60 @@
 import { Gateway, Network } from 'fabric-network';
-import { keys, values } from 'lodash';
+import { values } from 'lodash';
 import { evaluate, getNetwork, submitPrivateData } from '..';
+import { registerUser } from '../../account/registerUser';
 import { Commit } from '../../types';
-import set = Reflect.set;
 
-let config: {
-  network: Network;
-  gateway: Gateway;
-};
+let network: Network;
+let gateway: Gateway;
 let createdCommit_1: Commit;
 let createdCommit_2: Commit;
 const org1 = 'Org1PrivateDetails';
 const entityName = 'dev_test_privatedata';
-const id = 'privatedata_test_01';
 const transient = {
   eventstr: Buffer.from(
     JSON.stringify([{ type: 'Created', payload: { name: 'me' } }])
   )
 };
+const identity = `service_privatedata${Math.floor(Math.random() * 1000)}`;
 
 beforeAll(async () => {
-  config = await getNetwork();
+  try {
+    await registerUser({
+      enrollmentID: identity,
+      enrollmentSecret: 'password'
+    });
+    const config = await getNetwork({ identity });
+    network = config.network;
+    gateway = config.gateway;
+  } catch (error) {
+    console.error(error);
+    process.exit(-1);
+  }
 });
 
-afterAll(async () => await config.gateway.disconnect());
+afterAll(async () => await gateway.disconnect());
 
 describe('Event store Tests: Privatedata', () => {
   it('should createCommit #1', async () => {
     await submitPrivateData(
       'privatedata:createCommit',
-      [org1, entityName, id, '0'],
+      [org1, entityName, identity, '0'],
       transient,
-      { network: config.network }
+      { network }
     )
       .then<Commit>(result => values(result)[0])
       .then(commit => {
         createdCommit_1 = commit;
-        return expect(commit.entityId).toEqual(id);
+        return expect(commit.entityId).toEqual(identity);
       });
   });
 
   it('should createCommit #2', async () => {
     await submitPrivateData(
       'privatedata:createCommit',
-      [org1, entityName, id, '0'],
+      [org1, entityName, identity, '0'],
       transient,
-      { network: config.network }
+      { network }
     )
       .then<Commit>(result => values(result)[0])
       .then(commit => (createdCommit_2 = commit));
@@ -54,18 +63,18 @@ describe('Event store Tests: Privatedata', () => {
   it('should queryByEntityId #1', async () =>
     await evaluate(
       'privatedata:queryByEntityId',
-      [org1, entityName, id],
-      { network: config.network },
+      [org1, entityName, identity],
+      { network },
       true
     ).then(result =>
-      values(result).map(commit => expect(commit.id).toEqual(id))
+      values(result).map(commit => expect(commit.id).toEqual(identity))
     ));
 
   it('should queryByEntityName', async () =>
     await evaluate(
       'privatedata:queryByEntityName',
       [org1, entityName],
-      { network: config.network },
+      { network },
       true
     ).then(result =>
       values(result).map(commit =>
@@ -76,26 +85,24 @@ describe('Event store Tests: Privatedata', () => {
   it('should deleteByEntityIdCommitId #1', async () =>
     await submitPrivateData(
       'privatedata:deleteByEntityIdCommitId',
-      [org1, entityName, id, createdCommit_1.commitId],
+      [org1, entityName, identity, createdCommit_1.commitId],
       null,
-      { network: config.network }
-    ).then(result => expect(values(result)[0]).toEqual({})));
+      { network }
+    ).then(({ status }) => expect(status).toBe('SUCCESS')));
 
   it('should fail to deleteByEntityIdCommitId', async () =>
     await submitPrivateData(
       'privatedata:deleteByEntityIdCommitId',
-      [org1, entityName, id, createdCommit_1.commitId],
+      [org1, entityName, identity, createdCommit_1.commitId],
       null,
-      { network: config.network }
-    ).then(({ error }) =>
-      expect(error.message).toEqual('Endorsement has failed')
-    ));
+      { network }
+    ).then(({ status }) => expect(status).toBe('SUCCESS')));
 
   it('should deleteByEntityIdCommitId #2', async () =>
     await submitPrivateData(
       'privatedata:deleteByEntityIdCommitId',
-      [org1, entityName, id, createdCommit_2.commitId],
+      [org1, entityName, identity, createdCommit_2.commitId],
       null,
-      { network: config.network }
-    ).then(result => expect(values(result)[0]).toEqual({})));
+      { network }
+    ).then(({ status }) => expect(status).toBe('SUCCESS')));
 });
