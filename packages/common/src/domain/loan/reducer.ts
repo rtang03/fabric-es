39c0eac2ad766eccced01115fa7b3734ecae174d
pangduckwai <paul.lai@sea9.org>
2019-11-01 20:07:07 +0800

    Add loan to domain model

---
+++ packages/common/src/domain/loan/reducer.ts
@@ -0,0 +1,41 @@
+import { Loan, LoanEvent, LoanStatus } from '.';
+
+export const loanReducer = (
+  history: LoanEvent[],
+  initialState?: Loan
+): Loan => {
+  const reducer = (loan: Loan, event: LoanEvent): Loan => {
+    switch (event.type) {
+      case 'applied':
+        return {
+          loanId: event.payload.loanId,
+          status: LoanStatus[event.type],
+          ownerId: event.payload.userId,
+          timestamp: event.payload.timestamp,
+          loanProductId: null
+        };
+      case 'cancelled':
+      case 'approved':
+      case 'returned':
+      case 'rejected':
+      case 'expired':
+        // loan.status = LoanStatus[event.type];
+        return {
+          ...loan,
+          status: LoanStatus[event.type]
+        };
+      case 'LoanDescriptionDefined':
+        return {
+          ...loan,
+          description: event.payload.description
+        };
+      case 'LoanProductDefined':
+        return {
+          ...loan,
+          loanProductId: event.payload.loanProductId
+        };
+    }
+  };
+
+  return history.reduce(reducer, initialState);
+};
