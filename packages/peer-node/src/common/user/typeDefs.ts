14fbddf77f65a344ec18a22816b5ef9a3c47a497
pangduckwai <paul.lai@sea9.org>
2019-11-11 16:25:36 +0800

    Add private data in peer (WIP)

---
+++ packages/peer-node/src/common/user/typeDefs.ts
@@ -0,0 +1,41 @@
+import gql from 'graphql-tag';
+
+export const typeDefs = gql`
+type Query {
+  getCommitsByUserId(userId: String!): [UserCommit]!
+  getPaginatedUser(cursor: Int = 10): PaginatedUsers!
+  getUserById(userId: String!): User!
+  me: User
+}
+
+type Mutation {
+  createUser(name: String!, userId: String!): UserCommit
+}
+
+type User @key(fields: "userId") {
+  userId: String!
+  name: String!
+  mergedUserIds: [String!]
+}
+
+type PaginatedUsers {
+  entities: [User!]!
+  total: Int!
+  hasMore: Boolean!
+  otherInfo: [String!]!
+}
+
+type UserEvent {
+  type: String
+}
+
+type UserCommit {
+  id: String
+  entityName: String
+  version: Int
+  commitId: String
+  committedAt: String
+  entityId: String
+  events: [UserEvent!]
+}
+`;
