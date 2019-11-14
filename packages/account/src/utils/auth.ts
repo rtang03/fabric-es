a4cd4c918647de0c0c6f3cd0af2210780878d8da
tangross <rt.do.it@gmail.com>
2019-11-14 23:04:35 +0800

    can send authorization code

--- packages/account/src/utils/auth.ts
+++ packages/account/src/utils/auth.ts
@@ -1,14 +1,12 @@
 import { sign } from 'jsonwebtoken';
-import { OUser } from '../entity/OUser';
 import { User } from '../entity/User';
 
-export const createAccessToken = (user: User | OUser) => {
-  return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
+export const createAccessToken = (user: User) =>
+  sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
     expiresIn: '15m'
   });
-};
 
-export const createRefreshToken = (user: User | OUser) =>
+export const createRefreshToken = (user: User) =>
   sign(
     { userId: user.id, tokenVersion: user.tokenVersion },
     process.env.REFRESH_TOKEN_SECRET!,
--- packages/account/src/utils/createModel.ts
+++ packages/account/src/utils/createModel.ts
@@ -1,3 +1,4 @@
+import { sign } from 'jsonwebtoken';
 import {
   AuthorizationCode as IAuthorizationCode,
   Client as IClient,
@@ -10,14 +11,39 @@ import { AuthorizationCode } from '../entity/AuthorizationCode';
 import { Client } from '../entity/Client';
 import { OUser } from '../entity/OUser';
 import { RefreshToken } from '../entity/RefreshToken';
-import { createAccessToken, createRefreshToken } from './auth';
 
-export const model: Model = {
+export const createModel: (option?: {
+  accessTokenSecret: string;
+  accessTokenOptions: any;
+  refreshTokenSecret: string;
+  refreshTokenOptions: any;
+  authorizationCode: string;
+  authCodeOptions: any;
+}) => Model = (
+  option = {
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET!,
+    accessTokenOptions: { expiresIn: '15m' },
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET!,
+    refreshTokenOptions: { expiresIn: '7d' },
+    authorizationCode: process.env.AUTHORIZATION_CODE,
+    authCodeOptions: { expiresIn: '5m' }
+  }
+) => ({
   request: undefined,
   generateAccessToken: async (client: IClient, user: OUser, scope) =>
-    createAccessToken(user),
+    sign(
+      { userId: user.id },
+      option.accessTokenSecret,
+      option.accessTokenOptions
+    ),
   generateRefreshToken: async (client: IClient, user: OUser, scope) =>
-    createRefreshToken(user),
+    sign(
+      { userId: user.id },
+      option.refreshTokenSecret,
+      option.refreshTokenOptions
+    ),
+  generateAuthorizationCode: async (client: IClient, user: OUser, scope) =>
+    sign({ userId: user.id }, option.authorizationCode, option.authCodeOptions),
   getAccessToken: async (access_token: string) => {
     const token = await AccessToken.findOne({ access_token });
     const user = await OUser.findOne({ id: token.client_id });
@@ -26,24 +52,24 @@ export const model: Model = {
       accessToken: token.access_token,
       accessTokenExpiresAt: token.expires_at,
       client,
-      user
+      user: { id: user.id }
     };
   },
   getRefreshToken: async (refresh_token: string) => {
     const token = await RefreshToken.findOne({ refresh_token });
-    const user = await OUser.findOne({ id: token.client_id });
+    const user = await OUser.findOne({ id: token.user_id });
     const client = await Client.findOne({ id: token.client_id });
     return {
       refreshToken: token.refresh_token,
       refreshTokenExpiresAt: token.expires_at,
       scope: token.scope,
       client,
-      user
+      user: { id: user.id }
     };
   },
   getAuthorizationCode: async (authorization_code: string) => {
     const code = await AuthorizationCode.findOne({ authorization_code });
-    const user = await OUser.findOne({ id: code.client_id });
+    const user = await OUser.findOne({ id: code.user_id });
     const client = await Client.findOne({ id: code.client_id });
     return {
       authorizationCode: code.authorization_code,
@@ -51,29 +77,28 @@ export const model: Model = {
       redirectUri: code.redirect_uri,
       scope: code.scope,
       client,
-      user
+      user: { id: user.id }
     };
   },
   getClient: async (client_id: string, client_secret?: string) => {
     const client = client_secret
-      ? await Client.findOne({ client_id, client_secret }).catch(error =>
+      ? await Client.findOne({ id: client_id, client_secret }).catch(error =>
           console.error(error)
         )
-      : await Client.findOne({ client_id }).catch(error =>
+      : await Client.findOne({ id: client_id }).catch(error =>
           console.error(error)
         );
     return client
       ? {
           id: client.id,
           redirectUris: client.redirect_uris,
-          grants: client.grants
+          grants: client.grants,
+          user_id: client.user_id
         }
       : null;
   },
-  getUser: async (username: string, password: string) => {
-    // todo: check against the passsword salt
-    // todo: check against Fabric CA identity
-    return await OUser.findOne({ username });
+  getUser: async (id: string) => {
+    return await OUser.findOne({ id });
   },
   getUserFromClient: async (client: IClient) => {
     return OUser.findOne({ id: client.user_id });
@@ -88,12 +113,12 @@ export const model: Model = {
     return true;
   },
   revokeToken: async (token: IRefreshToken) => {
-    await RefreshToken.delete({ refresh_token: token.refreshToken }).catch(
-      error => {
-        console.error(error);
-        return false;
-      }
-    );
+    await RefreshToken.delete({
+      refresh_token: token.refreshToken
+    }).catch(error => {
+      console.error(error);
+      return false;
+    });
     return true;
   },
   saveAuthorizationCode: async (
@@ -105,7 +130,7 @@ export const model: Model = {
       authorization_code: code.authorizationCode,
       expires_at: code.expiresAt,
       redirect_uri: code.redirectUri,
-      scope: code.scope,
+      scope: code.scope || 'default',
       client_id: client.id,
       user_id: user.id
     };
@@ -116,7 +141,7 @@ export const model: Model = {
       redirectUri: authCode.redirect_uri,
       scope: authCode.scope,
       client,
-      user
+      user: { id: authCode.user_id }
     };
   },
   saveToken: async (token: IToken, client: IClient, user: OUser) => {
@@ -156,4 +181,4 @@ export const model: Model = {
     const authorizedScopes = token.scope.split(' ');
     return requestedScopes.every(s => authorizedScopes.indexOf(s) >= 0);
   }
-};
+});
