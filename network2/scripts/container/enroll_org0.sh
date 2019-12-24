7da8c3b8d2ec2bd82c435e844079d66499a16e48
Howin Ho <ho1ho@yahoo.com>
2019-12-24 16:02:47 +0800

    upgrade to Raft

--- network2/scripts/container/enroll_org0.sh
+++ network2/scripts/container/enroll_org0.sh
@@ -8,16 +8,48 @@ export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/ca/admin
 
 fabric-ca-client enroll -d -u https://rca-org0-admin:rca-org0-adminPW@0.0.0.0:6053
 fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6053
+fabric-ca-client register -d --id.name orderer2-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6053
+fabric-ca-client register -d --id.name orderer3-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6053
+fabric-ca-client register -d --id.name orderer4-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6053
+fabric-ca-client register -d --id.name orderer5-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6053
 fabric-ca-client register -d --id.name admin-org0 --id.secret org0AdminPW --id.type admin --id.attrs "hf.Registrar.Roles=client,hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert" -u https://0.0.0.0:6053
 
 # Copy Trusted Root Cert of org0 orderer1
 mkdir -p /var/artifacts/crypto-config/org0/orderer1/assets/ca
 cp /var/artifacts/crypto-config/org0/ca/admin/msp/cacerts/0-0-0-0-6053.pem /var/artifacts/crypto-config/org0/orderer1/assets/ca/org0-ca-cert.pem
+# Copy Trusted Root Cert of org0 orderer2
+mkdir -p /var/artifacts/crypto-config/org0/orderer2/assets/ca
+cp /var/artifacts/crypto-config/org0/ca/admin/msp/cacerts/0-0-0-0-6053.pem /var/artifacts/crypto-config/org0/orderer2/assets/ca/org0-ca-cert.pem
+# Copy Trusted Root Cert of org0 orderer3
+mkdir -p /var/artifacts/crypto-config/org0/orderer3/assets/ca
+cp /var/artifacts/crypto-config/org0/ca/admin/msp/cacerts/0-0-0-0-6053.pem /var/artifacts/crypto-config/org0/orderer3/assets/ca/org0-ca-cert.pem
+# Copy Trusted Root Cert of org0 orderer4
+mkdir -p /var/artifacts/crypto-config/org0/orderer4/assets/ca
+cp /var/artifacts/crypto-config/org0/ca/admin/msp/cacerts/0-0-0-0-6053.pem /var/artifacts/crypto-config/org0/orderer4/assets/ca/org0-ca-cert.pem
+# Copy Trusted Root Cert of org0 orderer5
+mkdir -p /var/artifacts/crypto-config/org0/orderer5/assets/ca
+cp /var/artifacts/crypto-config/org0/ca/admin/msp/cacerts/0-0-0-0-6053.pem /var/artifacts/crypto-config/org0/orderer5/assets/ca/org0-ca-cert.pem
 
 # Enroll orderer1
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer1/assets/ca/org0-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer1
 fabric-ca-client enroll -d -u https://orderer1-org0:ordererPW@0.0.0.0:6053
+# Enroll orderer2
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer2/assets/ca/org0-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer2
+fabric-ca-client enroll -d -u https://orderer2-org0:ordererPW@0.0.0.0:6053
+# Enroll orderer3
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer3/assets/ca/org0-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer3
+fabric-ca-client enroll -d -u https://orderer3-org0:ordererPW@0.0.0.0:6053
+# Enroll orderer4
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer4/assets/ca/org0-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer4
+fabric-ca-client enroll -d -u https://orderer4-org0:ordererPW@0.0.0.0:6053
+# Enroll orderer5
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer5/assets/ca/org0-ca-cert.pem
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer5
+fabric-ca-client enroll -d -u https://orderer5-org0:ordererPW@0.0.0.0:6053
 
 # Enroll Org0's Admin
 export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/admin
@@ -28,4 +60,20 @@ fabric-ca-client enroll -d -u https://admin-org0:org0AdminPW@0.0.0.0:6053
 # Copy admin cert to the org0 orderer1
 mkdir -p /var/artifacts/crypto-config/org0/orderer1/msp/admincerts
 cp /var/artifacts/crypto-config/org0/admin/msp/signcerts/cert.pem /var/artifacts/crypto-config/org0/orderer1/msp/admincerts/org0-admin-cert.pem
+
+# Copy admin cert to the org0 orderer2
+mkdir -p /var/artifacts/crypto-config/org0/orderer2/msp/admincerts
+cp /var/artifacts/crypto-config/org0/admin/msp/signcerts/cert.pem /var/artifacts/crypto-config/org0/orderer2/msp/admincerts/org0-admin-cert.pem
+
+# Copy admin cert to the org0 orderer3
+mkdir -p /var/artifacts/crypto-config/org0/orderer3/msp/admincerts
+cp /var/artifacts/crypto-config/org0/admin/msp/signcerts/cert.pem /var/artifacts/crypto-config/org0/orderer3/msp/admincerts/org0-admin-cert.pem
+
+# Copy admin cert to the org0 orderer4
+mkdir -p /var/artifacts/crypto-config/org0/orderer4/msp/admincerts
+cp /var/artifacts/crypto-config/org0/admin/msp/signcerts/cert.pem /var/artifacts/crypto-config/org0/orderer4/msp/admincerts/org0-admin-cert.pem
+
+# Copy admin cert to the org0 orderer5
+mkdir -p /var/artifacts/crypto-config/org0/orderer5/msp/admincerts
+cp /var/artifacts/crypto-config/org0/admin/msp/signcerts/cert.pem /var/artifacts/crypto-config/org0/orderer5/msp/admincerts/org0-admin-cert.pem
 
--- network2/scripts/container/enroll_tls.sh
+++ network2/scripts/container/enroll_tls.sh
@@ -9,6 +9,10 @@ fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW --id.type 
 fabric-ca-client register -d --id.name peer1-org2 --id.secret peer1PW --id.type peer -u https://0.0.0.0:6052
 fabric-ca-client register -d --id.name peer2-org2 --id.secret peer2PW --id.type peer -u https://0.0.0.0:6052
 fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6052
+fabric-ca-client register -d --id.name orderer2-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6052
+fabric-ca-client register -d --id.name orderer3-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6052
+fabric-ca-client register -d --id.name orderer4-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6052
+fabric-ca-client register -d --id.name orderer5-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:6052
 
 #############
 # org1 peer1#
@@ -83,11 +87,59 @@ mv /var/artifacts/crypto-config/org2/peer2/tls-msp/keystore/* /var/artifacts/cry
 mkdir -p /var/artifacts/crypto-config/org0/orderer1/assets/tls-ca
 cp /var/artifacts/crypto-config/ca-tls/admin/msp/cacerts/0-0-0-0-6052.pem /var/artifacts/crypto-config/org0/orderer1/assets/tls-ca/tls-ca-cert.pem
 
-# Enroll Org 0 Orderer 1
+# Enroll for org0 orderer1
 export FABRIC_CA_CLIENT_MSPDIR=tls-msp
 export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer1
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer1/assets/tls-ca/tls-ca-cert.pem
 fabric-ca-client enroll -d -u https://orderer1-org0:ordererPW@0.0.0.0:6052 --enrollment.profile tls --csr.hosts orderer1-org0
 
 mv /var/artifacts/crypto-config/org0/orderer1/tls-msp/keystore/* /var/artifacts/crypto-config/org0/orderer1/tls-msp/keystore/key.pem
+
+# Copy certificate of ca-tls for org0 orderer2
+mkdir -p /var/artifacts/crypto-config/org0/orderer2/assets/tls-ca
+cp /var/artifacts/crypto-config/ca-tls/admin/msp/cacerts/0-0-0-0-6052.pem /var/artifacts/crypto-config/org0/orderer2/assets/tls-ca/tls-ca-cert.pem
+
+# Enroll for org0 orderer2
+export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer2
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer2/assets/tls-ca/tls-ca-cert.pem
+fabric-ca-client enroll -d -u https://orderer2-org0:ordererPW@0.0.0.0:6052 --enrollment.profile tls --csr.hosts orderer2-org0
+
+mv /var/artifacts/crypto-config/org0/orderer2/tls-msp/keystore/* /var/artifacts/crypto-config/org0/orderer2/tls-msp/keystore/key.pem
+
+# Copy certificate of ca-tls for org0 orderer3
+mkdir -p /var/artifacts/crypto-config/org0/orderer3/assets/tls-ca
+cp /var/artifacts/crypto-config/ca-tls/admin/msp/cacerts/0-0-0-0-6052.pem /var/artifacts/crypto-config/org0/orderer3/assets/tls-ca/tls-ca-cert.pem
+
+# Enroll for org0 orderer3
+export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer3
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer3/assets/tls-ca/tls-ca-cert.pem
+fabric-ca-client enroll -d -u https://orderer3-org0:ordererPW@0.0.0.0:6052 --enrollment.profile tls --csr.hosts orderer3-org0
+
+mv /var/artifacts/crypto-config/org0/orderer3/tls-msp/keystore/* /var/artifacts/crypto-config/org0/orderer3/tls-msp/keystore/key.pem
+
+# Copy certificate of ca-tls for org0 orderer4
+mkdir -p /var/artifacts/crypto-config/org0/orderer4/assets/tls-ca
+cp /var/artifacts/crypto-config/ca-tls/admin/msp/cacerts/0-0-0-0-6052.pem /var/artifacts/crypto-config/org0/orderer4/assets/tls-ca/tls-ca-cert.pem
+
+# Enroll for org0 orderer4
+export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer4
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer4/assets/tls-ca/tls-ca-cert.pem
+fabric-ca-client enroll -d -u https://orderer4-org0:ordererPW@0.0.0.0:6052 --enrollment.profile tls --csr.hosts orderer4-org0
+
+mv /var/artifacts/crypto-config/org0/orderer4/tls-msp/keystore/* /var/artifacts/crypto-config/org0/orderer4/tls-msp/keystore/key.pem
+
+# Copy certificate of ca-tls for org0 orderer5
+mkdir -p /var/artifacts/crypto-config/org0/orderer5/assets/tls-ca
+cp /var/artifacts/crypto-config/ca-tls/admin/msp/cacerts/0-0-0-0-6052.pem /var/artifacts/crypto-config/org0/orderer5/assets/tls-ca/tls-ca-cert.pem
+
+# Enroll for org0 orderer5
+export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+export FABRIC_CA_CLIENT_HOME=/var/artifacts/crypto-config/org0/orderer5
+export FABRIC_CA_CLIENT_TLS_CERTFILES=/var/artifacts/crypto-config/org0/orderer5/assets/tls-ca/tls-ca-cert.pem
+fabric-ca-client enroll -d -u https://orderer5-org0:ordererPW@0.0.0.0:6052 --enrollment.profile tls --csr.hosts orderer5-org0
+
+mv /var/artifacts/crypto-config/org0/orderer5/tls-msp/keystore/* /var/artifacts/crypto-config/org0/orderer5/tls-msp/keystore/key.pem
 
