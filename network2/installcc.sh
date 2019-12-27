e0d4fbe66a26e023c74c846bb3afc7b99ee99167
Howin Ho <ho1ho@yahoo.com>
2019-12-27 17:23:22 +0800

    fixes

--- network2/installcc.sh
+++ network2/installcc.sh
@@ -5,12 +5,12 @@ yarn build
 # Install chaincode
 docker exec cli.org1.example.com sh -c "/setup/install_chaincode.sh org1.example.com peer0 eventstore 1.0 7051"
 docker exec cli.org1.example.com sh -c "/setup/install_chaincode.sh org1.example.com peer0 privatedata 1.0 7051"
-docker exec cli.org1.example.com sh -c "/setup/install_chaincode.sh org1.example.com peer1 eventstore 1.0 7051"
-docker exec cli.org1.example.com sh -c "/setup/install_chaincode.sh org1.example.com peer1 privatedata 1.0 7051"
-docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer0 eventstore 1.0 7051"
-docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer0 privatedata 1.0 7051"
-docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer1 eventstore 1.0 7051"
-docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer1 privatedata 1.0 7051"
+docker exec cli.org1.example.com sh -c "/setup/install_chaincode.sh org1.example.com peer1 eventstore 1.0 8051"
+docker exec cli.org1.example.com sh -c "/setup/install_chaincode.sh org1.example.com peer1 privatedata 1.0 8051"
+docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer0 eventstore 1.0 9051"
+docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer0 privatedata 1.0 9051"
+docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer1 eventstore 1.0 10051"
+docker exec cli.org2.example.com sh -c "/setup/install_chaincode.sh org2.example.com peer1 privatedata 1.0 10051"
 
 # Instantiate chaincode
 docker exec cli.org1.example.com sh -c "/setup/instantiate_chaincode_org1.sh"
