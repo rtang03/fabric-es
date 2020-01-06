d2ef16ea697486ac8c879e03674c20920b17e855
pangduckwai <paul.lai@sea9.org>
2020-01-06 08:52:43 +0800

    Revert sudo removal

--- network2/cleanup.sh
+++ network2/cleanup.sh
@@ -13,10 +13,10 @@ docker-compose -f ${_YAML_FILE} down --volumes
 docker volume prune -f
 docker network prune -f
 docker system prune -f
-rm -rf ${_HYPERLEDGER_DIR}
+sudo rm -rf ${_HYPERLEDGER_DIR}
 
 # Cleanup genesis block
-rm ${_FABRIC_DIR}/genesis.block
+sudo rm ${_FABRIC_DIR}/genesis.block
 
 # Cleanup chaincode images
 docker rmi -f $(docker images | grep fabcar | awk '{print $3}')
@@ -24,7 +24,7 @@ docker rmi -f $(docker images | grep eventstore | awk '{print $3}')
 docker rmi -f $(docker images | grep privatedata | awk '{print $3}')
 
 # Cleanup chaincode builds
-rm -rf ${_CHAINCODE_DIR}/dist
+sudo rm -rf ${_CHAINCODE_DIR}/dist
 # sudo rm -rf ${_CHAINCODE_DIR}/node_modules
 
 sleep 5
