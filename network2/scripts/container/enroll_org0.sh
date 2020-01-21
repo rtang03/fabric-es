1a4f74a9b9a01d1c5e9bfd40a9e5f133a6c609be
Howin Ho <ho1ho@yahoo.com>
2020-01-21 13:37:07 +0800

    cert passwords

--- network2/scripts/container/enroll_org0.sh
+++ network2/scripts/container/enroll_org0.sh
@@ -7,12 +7,12 @@ export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/ca/crypto/ca-c
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/ca/admin
 
 fabric-ca-client enroll -d -u https://rca-org0-admin:rca-org0-adminPW@0.0.0.0:5053
-fabric-ca-client register -d --id.name orderer1.example.com --id.secret ordererPW --id.type orderer -u https://0.0.0.0:5053
-fabric-ca-client register -d --id.name orderer2.example.com --id.secret ordererPW --id.type orderer -u https://0.0.0.0:5053
-fabric-ca-client register -d --id.name orderer3.example.com --id.secret ordererPW --id.type orderer -u https://0.0.0.0:5053
-fabric-ca-client register -d --id.name orderer4.example.com --id.secret ordererPW --id.type orderer -u https://0.0.0.0:5053
-fabric-ca-client register -d --id.name orderer5.example.com --id.secret ordererPW --id.type orderer -u https://0.0.0.0:5053
-fabric-ca-client register -d --id.name admin-example.com --id.secret OrdererOrgAdminPW --id.type admin --id.attrs "hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert" -u https://0.0.0.0:5053
+fabric-ca-client register -d --id.name orderer1.example.com --id.secret DPCxKv8m --id.type orderer -u https://0.0.0.0:5053
+fabric-ca-client register -d --id.name orderer2.example.com --id.secret PCzEE5x2 --id.type orderer -u https://0.0.0.0:5053
+fabric-ca-client register -d --id.name orderer3.example.com --id.secret T2ZNRe8x --id.type orderer -u https://0.0.0.0:5053
+fabric-ca-client register -d --id.name orderer4.example.com --id.secret 7B5qMkhg --id.type orderer -u https://0.0.0.0:5053
+fabric-ca-client register -d --id.name orderer5.example.com --id.secret p8Maufjr --id.type orderer -u https://0.0.0.0:5053
+fabric-ca-client register -d --id.name admin-example.com --id.secret sR7w9xWY --id.type admin --id.attrs "hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert" -u https://0.0.0.0:5053
 
 # Copy Trusted Root Cert of OrdererOrg orderer1
 mkdir -p /tmp/hyperledger/OrdererMSP/orderer1.example.com/assets/ca
@@ -33,29 +33,29 @@ cp /tmp/hyperledger/OrdererMSP/ca/admin/msp/cacerts/0-0-0-0-5053.pem /tmp/hyperl
 # Enroll orderer1
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/orderer1.example.com/assets/ca/example.com-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/orderer1.example.com
-fabric-ca-client enroll -d -u https://orderer1.example.com:ordererPW@0.0.0.0:5053
+fabric-ca-client enroll -d -u https://orderer1.example.com:DPCxKv8m@0.0.0.0:5053
 # Enroll orderer2
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/orderer2.example.com/assets/ca/example.com-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/orderer2.example.com
-fabric-ca-client enroll -d -u https://orderer2.example.com:ordererPW@0.0.0.0:5053
+fabric-ca-client enroll -d -u https://orderer2.example.com:PCzEE5x2@0.0.0.0:5053
 # Enroll orderer3
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/orderer3.example.com/assets/ca/example.com-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/orderer3.example.com
-fabric-ca-client enroll -d -u https://orderer3.example.com:ordererPW@0.0.0.0:5053
+fabric-ca-client enroll -d -u https://orderer3.example.com:T2ZNRe8x@0.0.0.0:5053
 # Enroll orderer4
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/orderer4.example.com/assets/ca/example.com-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/orderer4.example.com
-fabric-ca-client enroll -d -u https://orderer4.example.com:ordererPW@0.0.0.0:5053
+fabric-ca-client enroll -d -u https://orderer4.example.com:7B5qMkhg@0.0.0.0:5053
 # Enroll orderer5
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/orderer5.example.com/assets/ca/example.com-ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/orderer5.example.com
-fabric-ca-client enroll -d -u https://orderer5.example.com:ordererPW@0.0.0.0:5053
+fabric-ca-client enroll -d -u https://orderer5.example.com:p8Maufjr@0.0.0.0:5053
 
 # Enroll Org0's Admin
 export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/OrdererMSP/admin
 export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/OrdererMSP/orderer1.example.com/assets/ca/example.com-ca-cert.pem
 export FABRIC_CA_CLIENT_MSPDIR=msp
-fabric-ca-client enroll -d -u https://admin-example.com:OrdererOrgAdminPW@0.0.0.0:5053
+fabric-ca-client enroll -d -u https://admin-example.com:sR7w9xWY@0.0.0.0:5053
 
 # Copy admin cert to the OrdererOrg orderer1
 mkdir -p /tmp/hyperledger/OrdererMSP/orderer1.example.com/msp/admincerts
