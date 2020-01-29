a71dd04cae1109615a66208b18f394616127859b
Howin Ho <ho1ho@yahoo.com>
2020-01-29 16:43:09 +0800

    updated network

--- network2/scripts/container/join_channel_org3.sh
+++ network2/scripts/container/join_channel_org3.sh
@@ -15,3 +15,8 @@ export CORE_PEER_ADDRESS=peer1.hsbc.com.hk:7551
 peer channel join -b /var/artifacts/crypto-config/HsbcMSP/peer0.hsbc.com.hk/assets/loanapp.block
 peer channel getinfo -c loanapp
 
+# Update anchor peer
+peer channel update -c loanapp -f /var/artifacts/crypto-config/HsbcMSP/peer0.hsbc.com.hk/assets/hsbcAnchors.tx \
+    -o orderer0.hktfp.com:7050 \
+    --tls --cafile /var/artifacts/crypto-config/HsbcMSP/peer0.hsbc.com.hk/tls-msp/tlscacerts/tls-0-0-0-0-6052.pem
+
