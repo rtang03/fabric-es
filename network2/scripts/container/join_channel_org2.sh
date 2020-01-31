41fc7261864cf793811007c8ae35f65a01881607
tangross <rt.do.it@gmail.com>
2020-02-01 01:13:56 +0800

    revert to original network2

--- network2/scripts/container/join_channel_org2.sh
+++ network2/scripts/container/join_channel_org2.sh
@@ -1,22 +1,22 @@
 cp /var/artifacts/crypto-config/EtcMSP/peer0.etradeconnect.net/assets/loanapp.block /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/assets
 
 mkdir -p /var/artifacts/crypto-config/PbctfpMSP/admin/msp/admincerts
-cp /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/msp/admincerts/pbctfp-admin-cert.pem /var/artifacts/crypto-config/PbctfpMSP/admin/msp/admincerts
+cp /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/msp/admincerts/pbctfp.net-admin-cert.pem /var/artifacts/crypto-config/PbctfpMSP/admin/msp/admincerts
 
 export CORE_PEER_MSPCONFIGPATH=/var/artifacts/crypto-config/PbctfpMSP/admin/msp
 
 # peer0 joining the channel
-export CORE_PEER_ADDRESS=peer0-pbctfp:7251
+export CORE_PEER_ADDRESS=peer0.pbctfp.net:7251
 peer channel join -b /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/assets/loanapp.block
 peer channel getinfo -c loanapp
 
 # peer1 joining the channel
-export CORE_PEER_ADDRESS=peer1-pbctfp:7351
+export CORE_PEER_ADDRESS=peer1.pbctfp.net:7351
 peer channel join -b /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/assets/loanapp.block
 peer channel getinfo -c loanapp
 
 # Update anchor peer
 peer channel update -c loanapp -f /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/assets/pbctfpAnchors.tx \
-    -o orderer0-hktfp:7050 \
+    -o orderer0.hktfp.com:7050 \
     --tls --cafile /var/artifacts/crypto-config/PbctfpMSP/peer0.pbctfp.net/tls-msp/tlscacerts/tls-0-0-0-0-6052.pem
 
