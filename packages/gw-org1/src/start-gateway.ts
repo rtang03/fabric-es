49e104332f7ec481860d52620fa088900616207a
tangross <rt.do.it@gmail.com>
2020-02-06 10:00:45 +0800

    refactoring package.json

--- packages/gw-org1/src/start-gateway.ts
+++ packages/gw-org1/src/start-gateway.ts
@@ -2,18 +2,41 @@ require('./env');
 import { createGateway } from '@espresso/gw-node';
 
 const PORT = process.env.GATEWAY_PORT || 4001;
-const authenticationCheck = process.env.AUTHORIZATION_SERVER_URI || 'http://localhost:3301/oauth/authenticate';
+const authenticationCheck =
+  process.env.AUTHORIZATION_SERVER_URI ||
+  'http://localhost:3301/oauth/authenticate';
 
-(async () => {
+export const startGateway = async () => {
   const app = await createGateway({
     serviceList: [
-      { name: 'user',     url: `http://localhost:${process.env.SERVICE_USER_PORT}/graphql` },
-      { name: 'loan',     url: `http://localhost:${process.env.SERVICE_LOAN_PORT}/graphql` },
-      { name: 'document', url: `http://localhost:${process.env.SERVICE_DOCUMENT_PORT}/graphql` },
-      { name: 'private',  url: `http://localhost:${process.env.SERVICE_PRIVATE_PORT}/graphql` },
-      { name: 'remote-org2', url: `http://localhost:${process.env.REMOTE_ORG2_PORT}/graphql` },
-      { name: 'remote-org3', url: `http://localhost:${process.env.REMOTE_ORG3_PORT}/graphql` },
-      { name: 'admin', url: `http://localhost:${process.env.ADMINISTRATOR_PORT}/graphql` }
+      {
+        name: 'user',
+        url: `http://localhost:${process.env.SERVICE_USER_PORT}/graphql`
+      },
+      {
+        name: 'loan',
+        url: `http://localhost:${process.env.SERVICE_LOAN_PORT}/graphql`
+      },
+      {
+        name: 'document',
+        url: `http://localhost:${process.env.SERVICE_DOCUMENT_PORT}/graphql`
+      },
+      {
+        name: 'private',
+        url: `http://localhost:${process.env.SERVICE_PRIVATE_PORT}/graphql`
+      },
+      {
+        name: 'remote-org2',
+        url: `http://localhost:${process.env.REMOTE_ORG2_PORT}/graphql`
+      },
+      {
+        name: 'remote-org3',
+        url: `http://localhost:${process.env.REMOTE_ORG3_PORT}/graphql`
+      },
+      {
+        name: 'admin',
+        url: `http://localhost:${process.env.ADMINISTRATOR_PORT}/graphql`
+      }
     ],
     authenticationCheck,
     useCors: true,
@@ -22,7 +45,9 @@ const authenticationCheck = process.env.AUTHORIZATION_SERVER_URI || 'http://loca
   app.listen(PORT, () => {
     console.log(`🚀 Server at http://localhost:${PORT}/graphql`);
   });
-})().catch(error => {
+};
+
+startGateway().catch(error => {
   console.log(error);
   console.error(error.stack);
   process.exit(0);
