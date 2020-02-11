757cc56973a9ded40f6b2b8d34e539382375875c
Howin Ho <ho1ho@yahoo.com>
2020-02-11 12:18:25 +0800

    dev

--- deployments/app-net/dev/start.sh
+++ deployments/app-net/dev/start.sh
@@ -1,4 +1,4 @@
-docker-compose -f config/docker-compose.yaml up -d
-sleep 8
-docker-compose -f config/docker-compose.yaml up -d
+docker-compose -f config/db.yaml up -d
+sleep 20
+docker-compose -f config/auth.yaml up -d
 docker ps -a
