f47fda45125020687a52f3fabc6d4c4f3fce2b32
pangduckwai <paul.lai@sea9.org>
2020-02-21 14:00:27 +0800

    Add default value to logger

--- packages/gw-node/src/utils/getLogger.ts
+++ packages/gw-node/src/utils/getLogger.ts
@@ -2,50 +2,11 @@ import { LoggingWinston } from '@google-cloud/logging-winston';
 import { createLogger, format, Logger, transports } from 'winston';
 const { combine, timestamp, label, json } = format;
 
-// export const getLogger: (option: {
-//   name: string;
-//   sendToConsole?: boolean;
-//   enableGCPLogger?: boolean;
-// }) => Logger = ({
-//   name,
-//   sendToConsole = true,
-//   enableGCPLogger = process.env.NODE_ENV
-// }) => {
-//   let transportArray: any[] = [
-//     new transports.File({ filename: `./logs/all.log` }),
-//     new transports.File({
-//       filename: `./logs/error.log`,
-//       level: 'error'
-//     }),
-//     new transports.File({
-//       filename: `./logs/debug.log`,
-//       level: 'debug'
-//     }),
-//     new transports.File({
-//       filename: `./logs/warn.log`,
-//       level: 'warn'
-//     })
-//   ];
-
-//   const loggingWinston = new LoggingWinston({
-//     projectId: 'fdi'
-//   });
-
-//   const consoleTransport = new transports.Console();
-
-//   if (sendToConsole) transportArray = [consoleTransport, ...transportArray];
-
-//   if (enableGCPLogger) transportArray = [loggingWinston, ...transportArray];
-
-//   return createLogger({
-//     level: 'info',
-//     exitOnError: false,
-//     format: combine(label({ label: name }), timestamp(), json()),
-//     transports: transportArray
-//   });
-// };
-
-export const getLogger = (name: string, sendToConsole?: boolean, enableGCPLogger?: boolean) => {
+export const getLogger = (
+  name: string,
+  sendToConsole: boolean = true,
+  enableGCPLogger: boolean = process.env.NODE_ENV === 'production'
+) => {
   let transportArray: any[] = [
     new transports.File({ filename: `./logs/all.log` }),
     new transports.File({
