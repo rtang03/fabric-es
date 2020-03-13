c90c7106b6781addaa07be72000468ca8165879d
tangross <7686266+rtang03@users.noreply.github.com>
2020-03-13 12:32:15 +0800

    improve build image workflow #6 #7

--- _archive/lib-dev-net/scripts/gen_certs.sh
+++
@@ -1,4 +0,0 @@
-docker exec tls-ca-hktfp sh -c "/setup/enroll_tls.sh"
-docker exec rca-hktfp sh -c "/setup/enroll_org0.sh"
-docker exec rca-etradeconnect sh -c "/setup/enroll_org1.sh"
-docker exec rca-pbctfp sh -c "/setup/enroll_org2.sh"
