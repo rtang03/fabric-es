3dbd58d0c22215d98ce821bc809bdc53b7542b75
tangross <7686266+rtang03@users.noreply.github.com>
2020-03-17 10:33:55 +0800

    fix large file

--- deployments/dev-net/shutdown-cleanup.sh
+++ deployments/dev-net/shutdown-cleanup.sh
@@ -1,10 +1,14 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 export IMAGE_TAG=1.4.3
 export ARTIFACTS=./artifacts
 export SCRIPTS=./scripts
 export CHAINCODE=../../packages/chaincode
 export CONFIG=./config
 
-docker-compose -f compose.2org.yaml down
+docker-compose -f compose.1.2org.yaml down
 
 docker rm -f $(docker ps -aq -f status=exited)
 
