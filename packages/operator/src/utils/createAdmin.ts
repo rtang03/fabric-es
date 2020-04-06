3a17f2bb94d55105ffd63484f8bf477146fe7bad
tangross <7686266+rtang03@users.noreply.github.com>
2020-04-06 14:38:26 +0800

    fix unit-tests

--- packages/operator/src/utils/createAdmin.ts
+++ packages/operator/src/utils/createAdmin.ts
@@ -14,7 +14,7 @@ export const createAdmin = async (option: CreateAdminOption): Promise<Client.Use
   const privateKeyPath = `${orgAdminMspPath}/keystore/key.pem`;
   const signCertPath = `${orgAdminMspPath}/signcerts/cert.pem`;
 
-  logger.error(util.format('client => %j', client));
+  logger.error(util.format('client => %o', client));
 
   const mspid = client.getMspid();
 
