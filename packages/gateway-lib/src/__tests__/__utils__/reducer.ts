d0825874bd2d4e02301aaa2e721505a0c0a3077b
tangross <7686266+rtang03@users.noreply.github.com>
2020-05-11 11:00:32 +0800

    fix lint

--- packages/gateway-lib/src/__tests__/__utils__/reducer.ts
+++ packages/gateway-lib/src/__tests__/__utils__/reducer.ts
@@ -1,6 +1,6 @@
 import { Reducer } from '@fabric-es/fabric-cqrs';
-import { Counter, CounterEvent } from './types';
 import { CounterEvents } from './events';
+import { Counter, CounterEvent } from './types';
 
 export const reducer: Reducer<Counter> = (history: CounterEvent[], initial = { value: 0 }): Counter =>
   history.reduce(reducerFcn, initial);
