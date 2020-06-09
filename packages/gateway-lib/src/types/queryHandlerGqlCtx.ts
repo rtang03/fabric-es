5c368b236cfffb5d1dcf60d4997d4c194eada334
tangross <7686266+rtang03@users.noreply.github.com>
2020-06-10 02:26:33 +0800

    first ci attempt

--- packages/gateway-lib/src/types/queryHandlerGqlCtx.ts
+++ packages/gateway-lib/src/types/queryHandlerGqlCtx.ts
@@ -1,6 +1,6 @@
 import { QueryDatabase, QueryHandler } from '@fabric-es/fabric-cqrs';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
-import Redis from 'ioRedis';
+import Redis from 'ioredis';
 
 export interface QueryHandlerGqlCtx {
   pubSub?: RedisPubSub;
