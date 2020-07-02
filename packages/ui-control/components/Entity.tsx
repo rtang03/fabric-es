8f5fa9343b0aa9f96d063ca4d608c01444d72af4
tangross <7686266+rtang03@users.noreply.github.com>
2020-07-02 17:46:19 +0800

    byEntity pagination ui done

--- packages/ui-control/components/Entity.tsx
+++ packages/ui-control/components/Entity.tsx
@@ -1,5 +1,26 @@
+import { QueryHandlerEntity } from '@fabric-es/fabric-cqrs';
+import pick from 'lodash/pick';
 import React from 'react';
 
-const Entity: React.FC<any> = () => <div />;
+const Entity: React.FC<{ entities?: any[] }> = ({ entities }) => {
+  return (
+    <>
+      {entities ? (
+        entities
+          .map((entity) =>
+            pick(entity, 'id', 'entityName', 'tag', 'desc', 'created', 'lastModified')
+          )
+          .map((entity) => ({
+            ...entity,
+            created: new Date(entity?.created * 1000).toString(),
+            lastModified: new Date(entity?.lastModified * 1000).toString(),
+          }))
+          .map((entity) => <pre key={entity.id}>{JSON.stringify(entity, null, 2)}</pre>)
+      ) : (
+        <p>No data returned</p>
+      )}
+    </>
+  );
+};
 
 export default Entity;
