f2fed73f2e6b1a69bd82eed866f12474797ed3fd
cof <cofchan@gmail.com>
2020-07-13 09:55:56 +0800

    remove change 'Content-Type' to 'application/json'

--- packages/relay/src/relayService.ts
+++ packages/relay/src/relayService.ts
@@ -183,8 +183,8 @@ export const relayService = ({
       if (req.body) {
         const bodyData = JSON.stringify(req.body);
         // in case if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-        proxyReq.setHeader('Content-Type', 'application/json');
-        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // proxyReq.setHeader('Content-Type', 'application/json');
+        // proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
         // stream the content
         proxyReq.write(bodyData);
       }
