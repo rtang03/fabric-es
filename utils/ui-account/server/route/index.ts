a21aa615f37dfa490600a5370684ddc4d55aaef3
tangross <7686266+rtang03@users.noreply.github.com>
2020-07-25 18:09:25 +0800

    cleanup

--- utils/ui-account/server/route/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './createProfileRoute';
-export * from './createClientRoute';
-export * from './createApiKeyRoute';
-export * from './createIndexRoute';
-export * from './createGatewayRoute';
--- utils/ui-account/server/updateClientResponse.ts
+++
@@ -1,7 +0,0 @@
-export interface  UpdateClientResponse {
-  ok: boolean;
-  application_name: string;
-  client_secret: string;
-  grants: string[];
-  redirect_uris: string;
-}
