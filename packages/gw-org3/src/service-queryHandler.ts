cf43b9fc18ea7d54977a62278910ff937f1a0f64
pangduckwai <paul.lai@sea9.org>
2020-08-26 16:54:34 +0800

    Align redis retry strategy again

--- packages/gw-org3/src/service-queryHandler.ts
+++ packages/gw-org3/src/service-queryHandler.ts
@@ -18,7 +18,20 @@ const authCheck = process.env.AUTHORIZATION_SERVER_URI;
   const redisOptions: RedisOptions = {
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT, 10),
-    retryStrategy: (times) => Math.min(times * 50, 2000),
+    retryStrategy: (times) => {
+      if (times > 3) { // the 4th return will exceed 10 seconds, based on the return value...
+        logger.error(`Redis: connection retried ${times} times, exceeded 10 seconds.`);
+        process.exit(-1);
+      }
+      return Math.min(times * 100, 3000); // reconnect after (ms)
+    },
+    reconnectOnError: (err) => {
+      const targetError = 'READONLY';
+      if (err.message.includes(targetError)) {
+        // Only reconnect when the error contains "READONLY"
+        return 1;
+      }
+    }
   };
 
   const reducers = {
