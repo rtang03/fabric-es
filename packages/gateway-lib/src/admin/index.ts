5911593190a88e9136025c5ebb913fd056a8fac9
Ming KM Lau <ming.km.lau@hkicl.com.hk>
2021-10-28 16:48:08 +0800

    Changed add userRepo as optional

--- packages/gateway-lib/src/admin/index.ts
+++ packages/gateway-lib/src/admin/index.ts
@@ -1,5 +1,5 @@
-export * from './typeDefs';
-export * from './createResolvers';
 export * from './createAdminService';
 export * from './createAdminServiceWithAuth0';
+export * from './createResolvers';
 export * from './createResolversWithAuth0';
+export * from './typeDefs';
--- packages/gateway-lib/src/common/model/user/index.ts
+++ packages/gateway-lib/src/common/model/user/index.ts
@@ -3,22 +3,20 @@ import { CommandHandler, DataSrc } from '../../..';
 import { UserCommands } from './domain/commands';
 import { UserEvents } from './domain/events';
 import { User } from './domain/model';
-
-export * from './domain/model';
-export * from './domain/events';
-export * from './domain/indices';
 export * from './domain/commands';
+export * from './domain/events';
 export * from './domain/handler';
+export * from './domain/indices';
+export * from './domain/model';
 export * from './domain/reducer';
 export * from './domain/typeGuard';
-
-export { resolvers as userResolvers } from './service/resolvers';
-export * from './service/schema';
-
 export * from './query/createUser';
 export * from './query/getCommitsByUserId';
 export * from './query/getPaginatedUser';
 export * from './query/getUserById';
+export { resolvers as userResolvers } from './service/resolvers';
+export * from './service/schema';
+export { typeDefs as userTypeDefs } from './service/typeDefs';
 
 export type UserRepo = Repository<User, User, UserEvents>;
 export type UserCommandHandler = CommandHandler<UserCommands>;
